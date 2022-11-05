3542baf72d9f19cf9e3d6ce2ac9b5236e11e4c77
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-11-05 03:11:24 +0100

    Add Announcement SmartContract

--- README.md
+++ README.md
@@ -16,3 +16,45 @@ npm install truffle -g
 truffle compile
 truffle migrate
 ```
+
+### Contract usage
+
+In `truffle console`:
+
+```javascript
+// get array of ganache accounts
+let accounts = await web3.eth.getAccounts()
+
+// defining contract's instance
+let ann = await Announcement.deployed()
+```
+
+**Add announcement (price is in wei)**
+
+```javascript
+ann.add('title', 10, {from: accounts[0]})
+```
+
+Note: if you ommit `{from: <address>}`, the first one will be used by default.
+
+**Get announcement**
+
+```javascript
+// Getting announcement with id=0
+ann.anns(0)
+
+// Getting price of an announcement with id=0
+(await ann.anns(0))['price'].toNumber()
+```
+
+**Pay for an item**
+
+```javascript
+ann.pay(0, {from: accounts[2], value: '50'})
+```
+
+**Refund an item**
+
+```javascript
+ann.refund(0, {from: accounts[2], value: '50'})
+```
--- contracts/Announcement.sol
+++ contracts/Announcement.sol
@@ -1,13 +1,49 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 contract Announcement {
-  string description;
 
-  function readData() external view returns(string memory) {
-    return description;
+  struct Ann {
+    string name;
+    uint price;
+    address payable creator;
+    bool sold;
+    address payable buyer;
+    bool refunded;
   }
 
-  function updateData(string memory _description) external {
-    description = _description;
-  } 
+  Ann[] public anns;
+
+  function add(string calldata name, uint price) public {
+    anns.push(
+      Ann(name, price, payable(msg.sender), false, payable(0), false)
+    );
+  }
+
+  function pay(uint _index) public payable {
+    Ann storage ann = anns[_index];
+
+    require (!ann.sold, "Item already sold.");
+    require (msg.value == ann.price, "Incorrect value. Please send amount equal to announcement price.");
+
+		(bool success,) = ann.creator.call{value: msg.value}("");
+    require(success, "Failed to send money");
+
+    ann.sold = true;
+    ann.buyer = payable(msg.sender);
+  }
+
+  function refund(uint _index) public payable {
+    Ann storage ann = anns[_index];
+
+    require (ann.sold, "Item not sold.");
+    require (!ann.refunded, "Item already refunded.");
+    require (msg.sender == ann.creator, "Only announcement creator can refund money.");
+    require (msg.value == ann.price, "Incorrect value. Please send amount equal to announcement price.");
+
+		(bool success,) = ann.buyer.call{value: msg.value}("");
+    require(success, "Failed to send money");
+
+    ann.refunded = true;
+  }
 }
